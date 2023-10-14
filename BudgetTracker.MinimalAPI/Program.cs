9e759acfa6af6b93e6833ce01da2ee81e16749b2
evanRez <ereznice@gmail.com>
2023-10-14 17:43:43 -0400

    ADD_SWAGGER

--- BudgetTracker.MinimalAPI/Program.cs
+++ BudgetTracker.MinimalAPI/Program.cs
@@ -5,8 +5,17 @@ using BudgetTracker.MinimalAPI.RouteHandlers;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<BudgetTrackerDb>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapTransactionEndpoints();
 
